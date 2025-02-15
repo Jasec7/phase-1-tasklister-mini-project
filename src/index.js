6757a9585ae7f045374bf1c594fdd697fbe47619
Jasec <jasec_7@hotmail.com>
2025-02-15 02:55:10 -0800

    complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ // your code here
+ document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskDescription = document.querySelector('#new-task-description').value;
+    if (taskDescription) { 
+      createTask(taskDescription); 
+      document.querySelector('#new-task-description').value = '';
+    }
+  });
 });
+
+
+
+  switch (priority) {
+    case 'high':
+      p.style.color = red;
+      break;
+    case 'medium':
+      p.style.color = yellow;
+      break;
+    case 'low':
+      p.style.color = green;
+      break;
+  }
+
+
+function createTask(task) {
+  const p = document.createElement('p');
+  const btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  p.textContent = `${task} `;
+  p.appendChild(btn);
+  console.log(p);
+
+  const taskList = document.querySelector('#tasks');
+  if (taskList) { 
+    taskList.appendChild(p); 
+  } else {
+    console.error("Element with ID 'tasks' not found.");
+  }
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove(); 
+}
