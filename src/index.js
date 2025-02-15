2f73d1602386d5f04dc9a53f26c5bbb306368a1d
Jasec <jasec_7@hotmail.com>
2025-02-15 03:12:54 -0800

    complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -3,36 +3,35 @@
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     const taskDescription = document.querySelector('#new-task-description').value;
+    const priority = document.querySelector('#priority').value; 
     if (taskDescription) { 
-      createTask(taskDescription); 
+      createTask(taskDescription, priority); 
       document.querySelector('#new-task-description').value = '';
     }
   });
 });
 
-
+function createTask(task, priority) {
+  const p = document.createElement('p');
+  const btn = document.createElement('button');
 
   switch (priority) {
     case 'high':
-      p.style.color = red;
+      p.style.color = 'red';
       break;
     case 'medium':
-      p.style.color = yellow;
+      p.style.color = 'yellow'; 
       break;
     case 'low':
-      p.style.color = green;
+      p.style.color = 'green'; 
       break;
   }
 
-
-function createTask(task) {
-  const p = document.createElement('p');
-  const btn = document.createElement('button');
-  btn.addEventListener('click', handleDelete);
   btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+
   p.textContent = `${task} `;
-  p.appendChild(btn);
-  console.log(p);
+  p.appendChild(btn)
 
   const taskList = document.querySelector('#tasks');
   if (taskList) { 
